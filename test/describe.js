cc45d3d0f6c9b3efde843a23f19ea9eca1ed0518
André M. Arnold <andre.m.arnold@gmail.com>
2016-10-16 14:55:09 +0200

    add:describe.many accepts objects.

--- test/describe.js
+++ test/describe.js
@@ -1,8 +1,20 @@
 import tape from "tape"
 
+const firstPair = o => {
+	const owns = Object.prototype.hasOwnProperty
+	let k
+	for (k in o) break
+	k = k != null && owns.call(o, k) ? k : null
+	return [k, k == null ? void 0 : o[k]]
+}
+
 const noMore = (msg, cb) => tape.skip(msg, cb)
 const only = (msg, cb) => tape.only(msg, cb)
-const many = (...tests) => tests.forEach(t => t(describe))
+const many = (...tests) =>
+	tests.forEach(t => typeof t === "function"
+		? t(describe)
+		: describe.apply(void 0, firstPair(t))
+	)
 
 const describe = (tape => {
 	function wrapEnsure (t) { return (msg, cb) => t.test("it " + msg, cb) }
